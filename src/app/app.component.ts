971580cdfca35c48bc0b7f6baa453608517202bf
Eldho_Thomas <eldho.thomas@gmail.com>
2021-05-18 19:01:32 -0500

    Console.log works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 
-
 export interface Employee {
   empId: string,
   workDays: number,
   vacationdays: number
 }
-
 
 @Component({
   selector: 'app-root',
@@ -26,10 +24,11 @@ export class AppComponent implements OnInit{
     console.log("Calling API from component - " + empInfo.empId);
     this.apiService.callApi(empInfo.empId)
     .subscribe (
-      (data: Employee) => this.employee  = { ...data }
+      (data: Employee) => {
+        this.employee  = { ...data };
+        console.log('Return JSON for emp id ' +  this.employee?.empId + JSON.stringify(this.employee));
+      }
       )
-      console.log('Return JSON for emp id ' +  this.employee?.empId + JSON.stringify(this.employee));
-
   }
 
   ngOnInit() {}
